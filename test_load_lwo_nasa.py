701f1ec8b47c132d3e6de9868b86979edea58fcc
Douglas Kastle <douglas.kastle@gmail.com>
2020-02-12 17:08:15 +0000

    First checkin

---
+++ test_load_lwo_nasa.py
@@ -0,0 +1,7 @@
+from lwo_helper import load_lwo
+
+def test_load_nasa0():
+    #infile = "tests/lwo_nasa/src/Advanced Composition/ACE-Composite-2011-3.lwo"
+    infile = "tests/lwo_nasa/src/Argo/Argo.lwo"
+    load_lwo(infile)
+
